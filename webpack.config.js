582ce625a4f9e5bf95a82674a5552f97e9e59eb6
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-01-18 00:01:09 -0200

    configurado webpack para fazer a compilação do projeto

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.jsx',
+  output: {
+    path: path.resolve(__dirname, '/public'),
+    filename: './app.js',
+  },
+  devServer: {
+    port: 8080,
+    contentBase: './src',
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+    alias: {
+      modules: path.resolve(__dirname, '/node_modules'),
+      jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
+      bootstrap: 'modules/admin-lte/plugins/bootstrap/bootstrap.js',
+    },
+    plugins: [
+      new webpack.ProviderPlugin({
+        $: 'jquery',
+        jQuery: 'jquery',
+        'window.jQuery': 'jquery',
+      }),
+      new ExtractTextPlugin('app.css'),
+    ],
+    module: {
+      loaders: [{
+        test: /.js[x]?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: ['transform-object-rest-spread'],
+        },
+      }, {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
+      }, {
+        test: /\.woff|.woof2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+        loader: 'file',
+      }],
+    },
+  },
+};
