2f89d98a2d627cf10cd20232ea6048f70ad1080c
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-01-31 15:48:17 -0200

    adicionado componente footer

---
+++ src/common/template/footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default props => (
+  <footer className="main-footer">
+    <strong>
+      Copyright &copy; 2019 &nbsp;
+      <a href="https://github.com/laugustofrontend" target="_blank">
+        laugutofrontend
+      </a>
+    </strong>
+  </footer>
+);
