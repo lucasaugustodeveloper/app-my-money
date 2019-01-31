1bd91c7b6bc8da2dc003ac560dc318e3063cb973
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-01-31 14:10:37 -0200

    alterado webpack

--- webpack.config.js
+++ webpack.config.js
@@ -5,44 +5,48 @@ const path = require('path');
 module.exports = {
   entry: './src/index.jsx',
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: './app.js',
+    path: path.resolve(__dirname, '/public'),
+    filename: './app.js'
   },
   devServer: {
     port: 8080,
-    contentBase: './public',
+    contentBase: './public'
   },
   resolve: {
     extensions: ['', '.js', '.jsx'],
     alias: {
       modules: path.resolve(__dirname, '/node_modules'),
       jquery: 'modules/admin-lte/plugins/jQuery/jquery-2.2.3.min.js',
-      bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js',
-    },
+      bootstrap: 'modules/admin-lte/bootstrap/js/bootstrap.js'
+    }
   },
   plugins: [
     new webpack.ProvidePlugin({
       $: 'jquery',
       jQuery: 'jquery',
-      'window.jQuery': 'jquery',
+      'window.jQuery': 'jquery'
     }),
-    new ExtractTextPlugin('app.css'),
+    new ExtractTextPlugin('app.css')
   ],
   module: {
-    loaders: [{
-      test: /.js[x]?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      query: {
-        presets: ['es2015', 'react'],
-        plugins: ['transform-object-rest-spread'],
+    loaders: [
+      {
+        test: /.js[x]?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+          plugins: ['transform-object-rest-spread']
+        }
       },
-    }, {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract('style-loader', 'css-loader'),
-    }, {
-      test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
-      loader: 'file',
-    }],
-  },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+      },
+      {
+        test: /\.woff|.woff2|.ttf|.eot|.svg|.png|.jpg*.*$/,
+        loader: 'file'
+      }
+    ]
+  }
 };
