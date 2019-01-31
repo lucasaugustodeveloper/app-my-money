97694f32c2639383f120d2b694efcedd926df23f
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-01-31 15:21:23 -0200

    adicionado componente header no projeto

---
+++ src/common/template/header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default props => (
+  <header className="main-header">
+    <a href="/#/" className="logo">
+      <span className="logo-mini">
+        <strong>My</strong> M
+      </span>
+      <span className="logo-lg">
+        <span className="fa fa-money" />
+        <strong>My</strong> M
+      </span>
+    </a>
+
+    <nav className="navbar static-top">
+      <a href className="sidebar-toggle" data-toggle="offcanvas" />
+    </nav>
+  </header>
+);
