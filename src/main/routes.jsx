611855d6bdc1fa20c513293a784f4eb1dd3c447c
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-02-01 16:09:15 -0200

    consertado erro de digitação dos componentes exportados

--- src/main/routes.jsx
+++ src/main/routes.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import { Router, Route, Redirect, hasHistory } from 'react-router';
+import { Router, Route, Redirect, hashHistory } from 'react-router';
 
 import Dashboard from '../dashboard/dashboard';
 import BillingCycle from '../billingCycle/billingCycle';
 
 export default props => (
-  <Router history={hasHistory}>
+  <Router history={hashHistory}>
     <Route path="/" exact component={Dashboard} />
-    <Route path="/billingCycles" component={BillingCycle} />
+    <Route path="billingCycles" component={BillingCycle} />
     <Redirect from="*" to="/" />
   </Router>
 );
