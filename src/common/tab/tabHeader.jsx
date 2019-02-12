15bc379f154f1db7a84c1da9e5ebda5c079db386
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-02-11 23:46:30 -0200

    adicionando componente de tab para fazer crud de valores

---
+++ src/common/tab/tabHeader.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class TabHeader extends Component {
+  render() {
+    return (
+      <li>
+        <a href="javascript:;" data-toggle="tab" data-target={this.props.target}>
+          <span className={`fa fa-${this.props.icon}`} />
+          {` ${this.props.label}`}
+        </a>
+      </li>
+    );
+  }
+}
+
+export default TabHeader;
