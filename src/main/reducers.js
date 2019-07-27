be2817f693647b0f96b39ebb884881422e1ec810
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-07-26 22:20:52 -0300

    Create and add component of list

--- src/main/reducers.js
+++ src/main/reducers.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 
 import dashboardReducer from '../dashboard/dashboardReduce';
 import TabReducer from '../common/tab/tabReducer';
+import BillingCycleReducer from '../billingCycle/billingCycleReducer';
 
 const rootReducers = combineReducers({
   dashboard: dashboardReducer,
   tab: TabReducer,
+  billingCycle: BillingCycleReducer
 });
 
 export default rootReducers;
