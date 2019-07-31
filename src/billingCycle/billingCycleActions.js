18744d1c70122758b886aafdf35fc19f62628d49
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-07-31 20:07:13 -0300

    Connected form with redux

--- src/billingCycle/billingCycleActions.js
+++ src/billingCycle/billingCycleActions.js
@@ -1,8 +1,10 @@
 import { toastr } from 'react-redux-toastr';
-import { reset as resetForm } from 'redux-form';
+import { reset as resetForm, initialize } from 'redux-form';
 import { showTabs, selectTab } from '../common/tab/tabActions';
 
 import api from '../services/api';
+
+const INITIAL_VALUES = [];
 
 export function getList() {
   const request = api.get('/billingCycles');
@@ -13,18 +15,22 @@ export function getList() {
   };
 }
 
+export function init() {
+  return [
+    showTabs('tabList', 'tabCreate'),
+    selectTab('tabList'),
+    getList(),
+    initialize('billingCycleForm', INITIAL_VALUES)
+  ];
+}
+
 export function create(values) {
   return dispatch => {
     api
       .post('/billingCycles', values)
       .then(res => {
         toastr.success('Sucess', 'Operação realiza com sucesso!');
-        dispatch([
-          resetForm('billingCycleForm'),
-          getList(),
-          selectTab('tabList'),
-          showTabs('tabList', 'tabCreate')
-        ]);
+        dispatch(init());
       })
       .catch(e => {
         e.response.data.errors.forEach(error => toastr.error('Erro', error));
@@ -33,5 +39,9 @@ export function create(values) {
 }
 
 export function showUpdate(billingCycle) {
-  return [showTabs('tabUpdate'), selectTab('tabUpdate')];
+  return [
+    showTabs('tabUpdate'),
+    selectTab('tabUpdate'),
+    initialize('billingCycleForm', billingCycle)
+  ];
 }
