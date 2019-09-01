43e85824d87ea76a423ad9ca7c4676aaa4deda4e
Lucas Augusto <lucas.augusto5061@gmail.com>
2019-09-01 16:17:03 -0300

    Create and add component custom input

--- src/billingCycle/billingCycleForm.jsx
+++ src/billingCycle/billingCycleForm.jsx
@@ -36,7 +36,7 @@ class BillingCycleForm extends Component {
           />
         </div>
 
-        <CreditList cols='12 6'/>
+        <CreditList cols='12 6' readOnly={readOnly} />
 
         <div className='box-footer'>
           <button type='submit' className={`btn btn-${this.props.submitClass}`}>
